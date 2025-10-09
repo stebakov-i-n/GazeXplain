ce0777115327ec2920a40432218e9b31739fc7fc
stebakov-i-n <chester50796@yandex.ru>
2025-10-09 15:11:29 +0000

    Init

--- train.py
+++ train.py
@@ -6,34 +6,34 @@ task = Task.init()
 
 print('Test task')
 
-dataset = Dataset.get(dataset_name='GazeXplain_dataset', dataset_project='GazeXplain')
-dataset_path = dataset.get_local_copy()
-print(f'Dataset downloaded to: {dataset_path}')
+# dataset = Dataset.get(dataset_name='GazeXplain_dataset', dataset_project='GazeXplain')
+# dataset_path = dataset.get_local_copy()
+# print(f'Dataset downloaded to: {dataset_path}')
 
-# # Инициализация задачи ClearML
-# task = Task.init(
-#     project_name='GazeXplain',
-#     task_name='GazeXplain_Training',
-#     auto_connect_frameworks=True
-# )
+# # # Инициализация задачи ClearML
+# # task = Task.init(
+# #     project_name='GazeXplain',
+# #     task_name='GazeXplain_Training',
+# #     auto_connect_frameworks=True
+# # )
 
-# Логируем сам sh-скрипт
-task.upload_artifact(name='training_script', artifact_object='GazeXplain/bash/train.sh')
+# # Логируем сам sh-скрипт
+# task.upload_artifact(name='training_script', artifact_object='GazeXplain/bash/train.sh')
 
-# Запускаем shell-скрипт
-try:
-    result = subprocess.run(
-        ['bash', 'GazeXplain/bash/train.sh'],
-        capture_output=True,
-        text=True,
-        check=True
-    )
+# # Запускаем shell-скрипт
+# try:
+#     result = subprocess.run(
+#         ['bash', 'GazeXplain/bash/train.sh'],
+#         capture_output=True,
+#         text=True,
+#         check=True
+#     )
 
-    task.upload_artifact('trained_model', artifact_object='runs/COCO_TP_runX_baseline/checkpoints/ckpt_best/model.safetensors')
-    task.upload_artifact('evaluation', artifact_object='runs/COCO_TP_runX_baseline/validation/metric.json')
+#     task.upload_artifact('trained_model', artifact_object='runs/COCO_TP_runX_baseline/checkpoints/ckpt_best/model.safetensors')
+#     task.upload_artifact('evaluation', artifact_object='runs/COCO_TP_runX_baseline/validation/metric.json')
 
 
         
-except subprocess.CalledProcessError as e:
-    task.get_logger().report_text(f"Script failed: {e}")
-    raise
+# except subprocess.CalledProcessError as e:
+#     task.get_logger().report_text(f"Script failed: {e}")
+#     raise
