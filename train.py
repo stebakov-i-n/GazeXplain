2e156a26b3b8dec1291fabaa851725fb8c2d7f2d
stebakov-i-n <chester50796@yandex.ru>
2025-10-09 17:50:02 +0300

    Init

---
+++ train.py
@@ -0,0 +1,37 @@
+from clearml import Task, Dataset
+import subprocess
+import os
+
+task = Task.init()
+
+dataset = Dataset.get(dataset_name='GazeXplain_dataset', dataset_project='GazeXplain')
+dataset_path = dataset.get_local_copy()
+print(f'Dataset downloaded to: {dataset_path}')
+
+# # Инициализация задачи ClearML
+# task = Task.init(
+#     project_name='GazeXplain',
+#     task_name='GazeXplain_Training',
+#     auto_connect_frameworks=True
+# )
+
+# Логируем сам sh-скрипт
+task.upload_artifact(name='training_script', artifact_object='GazeXplain/bash/train.sh')
+
+# Запускаем shell-скрипт
+try:
+    result = subprocess.run(
+        ['bash', 'GazeXplain/bash/train.sh'],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+
+    task.upload_artifact('trained_model', artifact_object='runs/COCO_TP_runX_baseline/checkpoints/ckpt_best/model.safetensors')
+    task.upload_artifact('evaluation', artifact_object='runs/COCO_TP_runX_baseline/validation/metric.json')
+
+
+        
+except subprocess.CalledProcessError as e:
+    task.get_logger().report_text(f"Script failed: {e}")
+    raise
