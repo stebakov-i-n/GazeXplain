2a591c107db3689b078bc80ee0ee098337a71cff
stebakov-i-n <chester50796@yandex.ru>
2025-10-10 12:10:00 +0000

    Add dataset load

--- train.py
+++ train.py
@@ -6,9 +6,16 @@ task = Task.init()
 
 print('Test task')
 
-# dataset = Dataset.get(dataset_name='GazeXplain_dataset', dataset_project='GazeXplain')
-# dataset_path = dataset.get_local_copy()
-# print(f'Dataset downloaded to: {dataset_path}')
+dataset = Dataset.get(dataset_name='GazeXplain_dataset', dataset_project='GazeXplain')
+dataset_path = dataset.get_local_copy()
+print(f'Dataset downloaded to: {dataset_path}')
+
+subprocess.run(
+    ['bash', 'GazeXplain/bash/train.sh'],
+    capture_output=True,
+    text=True,
+    check=True
+)
 
 # # # Инициализация задачи ClearML
 # # task = Task.init(
